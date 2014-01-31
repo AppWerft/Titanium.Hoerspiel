6ae384afa35df12a21d0bf2ebb876b91087b383f
AppWerft <rs@hamburger-appwerft.de>
2014-01-31 20:59:33 +0100

    V41

---
+++ Resources/ui/headerview.widget.js
@@ -0,0 +1,41 @@
+exports.create = function(_options){
+	console.log(_options);
+	var self = Ti.UI.createView({
+		height : '36dp',
+		backgroundColor : 'gray'
+	});
+	if (_options.key == 'cached') {
+		var quota = (Ti.App.Model.getQuota()) ? Ti.App.Model.getQuota().quota : 0.02;
+		var quotaView = Ti.UI.createProgressBar({
+			min : 0,
+			max : 1,
+			value : quota,
+			bottom : '5dp',
+			height : '30dp',
+			width : Ti.UI.FILL,
+			left : '200dp',
+			right : '10dp'
+		});
+		Ti.App.addEventListener('quota', function(_evt) {
+			quotaView.setValue(_evt.quota.quota);
+		});
+		self.add(quotaView);
+		quotaView.show();
+	}
+	self.add(Ti.UI.createLabel({
+		left : '40dp',
+		textAlign : 'left',
+		color : 'white',
+		font : {
+			fontWeight : 'bold'
+		},
+		text : _options.title
+	}));
+	self.add(Ti.UI.createImageView({
+		left : '5dp',
+		width : '25dp',
+		height : '25dp',
+		image : '/images/' + _options.key + '.png'
+	}));
+	return self;
+};
